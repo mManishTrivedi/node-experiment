cc30ec1e71babcfdb9c382da580e4f44b9f795a8
Manish <manish@readybytes.in>
2014-05-23 20:06:08 +0530

    Task :: Load html css and js page.

--- index.js
+++ index.js
@@ -2,29 +2,94 @@
 // define basic stuff
 var port 		= 4444;
 var serverUrl	= "127.0.0.1";
-var filePath	= __dirname + '/template/mani/index.html';
+var dirPath		= __dirname ;
  
 var http = require("http");
 var fs = require("fs");
 
 console.log(" Web-Server is running at " + serverUrl + ":" + port);
 
+// String function define
+if ( typeof String.prototype.endsWith != 'function' ) {
+	  String.prototype.endsWith = function( str ) {
+	    return str.length > 0 && this.substring( this.length - str.length, this.length ) === str;
+	  }
+	};
+	
+	
+
 // create server
 http.createServer(function(request,response	) {
+	
+	var filePath = request.url;
+	
+	var error_file_not_exist = 
+		function (filePath,err) 
+		{
+			console.log(err + '## ' + filePath + ' File is not Exist... :( ');
+			
+			response.writeHead(500);
+			response.end('File is not Exist... :( ');
+		};
+	
 	//logs display on cli
 	console.log('web-server initiate');
 	
-	fs.readFile(filePath, function(err, contents) {
-		if(!err) {
-			response.setHeader("Content-Length", contents.length);
-			response.setHeader("Content-Type", 'text/html');
-			response.statusCode = 200;
-			response.end(contents);
-		} else {
-			response.writeHead(500);
-			response.end();
-		}
-		});
+	if	(filePath.endsWith('.js'))	{ //filePath has the pathname, check if it conatins '.js'
+		
+		
+		console.log('load js file' + filePath);
+		
+	      fs.readFile(dirPath + filePath, function (err, contents) {
+	        if (err) { 
+	        	error_file_not_exist(dirPath + filePath, err);
+	        }
+	        
+	        response.writeHead(200, {'Content-Type': 'text/javascript'});
+	        response.write(contents);
+	        response.end();
+	      });
+
+	    }
+	
+	
+	if	(filePath.endsWith('.css'))	{	//filePath has the pathname, check if it conatins '.css'
+		
+		
+		console.log('load css file' + filePath);
+		
+	      fs.readFile(dirPath + filePath, function (err, contents) {
+	        if (err) { 
+	        	error_file_not_exist(dirPath + filePath, err);
+	        }
+	        
+	        response.writeHead(200, {'Content-Type': 'text/css'});
+	        response.write(contents);
+	        response.end();
+	      });
+
+	    }
+
+	
+	
+		
+	//default : filePath has the pathname, check if it conatins '.html'
+	if	(!filePath.endsWith('.css') && !filePath.endsWith('.js'))	{	
+		
+		filePath = dirPath + '/template/career/index.html';
+		
+		fs.readFile(filePath, function(err, contents) {
+			if(!err) {
+				response.setHeader("Content-Length", contents.length);
+				response.setHeader("Content-Type", 'text/html');
+				response.statusCode = 200;
+				response.end(contents);
+			} else {
+				
+				error_file_not_exist(filePath, err);
+			}
+			});
+	}
 	
 	
 	//response.writeHead(200, {'Content-Type': 'text/plain'});
