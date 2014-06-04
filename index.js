a37c7fdc207823770b3e8afb59b1e2aad8242970
Manish <manish@readybytes.in>
2014-06-04 18:23:34 +0530

    Change structure

--- index.js
+++ index.js
@@ -2,10 +2,12 @@
 // define basic stuff
 var port 		= 4444;
 var serverUrl	= "127.0.0.1";
-var dirPath		= __dirname ;
+var dirPath		= __dirname + '/app/' ;
  
-var http = require("http");
-var fs = require("fs");
+var http	= require("http");
+var fs		= require("fs");
+var url 	= require("url");
+var qs = require('querystring');
 
 console.log(" Web-Server is running at " + serverUrl + ":" + port);
 
@@ -21,79 +23,84 @@ if ( typeof String.prototype.endsWith != 'function' ) {
 // create server
 http.createServer(function(request,response	) {
 	
-	var filePath = request.url;
+	var filePath =   request.url;
 	
+	var pathname 	= 	url.parse(filePath).pathname;
+	var get_var		=	url.parse(filePath, true).query;
+	
+	//console.log(get_var);
+	
+	console.error("TODO:: Pathname"+ pathname);
+	console.error(get_var['form']);
+	
+	if ('/career' === pathname && get_var['form']) {
+		response.writeHead(200, {'content-type': 'text/plain'});
+		var objToJson = { };
+		objToJson.response = 'Yo buddy!good job';
+		response.write(JSON.stringify(objToJson));
+		response.end();
+		return
+	}
+	
+	//logs display on cli
+	console.log('web-server initiate and req-url is :'+ filePath);
+
 	var error_file_not_exist = 
 		function (filePath,err) 
 		{
 			console.log(err + '## ' + filePath + ' File is not Exist... :( ');
 			
 			response.writeHead(500);
-			response.end('File is not Exist... :( ');
+			response.end(' File is not Exist... :( ');
 		};
-	
-	//logs display on cli
-	console.log('web-server initiate');
-	
-	if	(filePath.endsWith('.js'))	{ //filePath has the pathname, check if it conatins '.js'
 		
-		
-		console.log('load js file' + filePath);
-		
-	      fs.readFile(dirPath + filePath, function (err, contents) {
-	        if (err) { 
-	        	error_file_not_exist(dirPath + filePath, err);
-	        }
-	        
-	        response.writeHead(200, {'Content-Type': 'text/javascript'});
-	        response.write(contents);
-	        response.end();
-	      });
-
-	    }
+	//default stuff
+	var file_type = 'html';
+	var content_type = 'text/html';
 	
 	
-	if	(filePath.endsWith('.css'))	{	//filePath has the pathname, check if it conatins '.css'
-		
-		
-		console.log('load css file' + filePath);
-		
-	      fs.readFile(dirPath + filePath, function (err, contents) {
-	        if (err) { 
-	        	error_file_not_exist(dirPath + filePath, err);
-	        }
-	        
-	        response.writeHead(200, {'Content-Type': 'text/css'});
-	        response.write(contents);
-	        response.end();
-	      });
-
-	    }
-
+	if	(filePath.endsWith('.js'))	{
+		file_type = 'js';	
+	}
 	
+	if	(filePath.endsWith('.css'))	{
+		file_type = 'css';	
+	}
 	
-		
-	//default : filePath has the pathname, check if it conatins '.html'
-	if	(!filePath.endsWith('.css') && !filePath.endsWith('.js'))	{	
-		
-		filePath = dirPath + '/template/career/index.html';
-		
-		fs.readFile(filePath, function(err, contents) {
-			if(!err) {
-				response.setHeader("Content-Length", contents.length);
-				response.setHeader("Content-Type", 'text/html');
-				response.statusCode = 200;
-				response.end(contents);
-			} else {
-				
-				error_file_not_exist(filePath, err);
-			}
-			});
+	if	(filePath.endsWith('.jpg') || filePath.endsWith('.png') || filePath.endsWith('.jepg') ||filePath.endsWith('.gif'))	{
+		file_type = 'img';	
+	}
+	
+	filePath = dirPath + filePath;
+	
+	switch (file_type)
+	{
+		case 'js' :
+			content_type = 'text/javascript';
+			break;
+		case 'css' :
+			content_type = 'text/css';
+			break;
+		case 'img' :
+			content_type = 'text/gif';
+			break;
+		case 'html' :
+		default:
+			filePath = dirPath + '/career/index.html';
+			content_type = 'text/html';
+			break;
 	}
 	
 	
-	//response.writeHead(200, {'Content-Type': 'text/plain'});
-	
-    //response.end("it's me, Manish Trivedi");
+	fs.readFile(filePath, function (err, contents) {
+        if (err) { 
+        	error_file_not_exist(filePath, err);
+        }
+        
+        response.writeHead(200, {'Content-Type': content_type});
+        response.write(contents);
+        response.end();
+      });
     
+	
 }).listen(port, serverUrl);
